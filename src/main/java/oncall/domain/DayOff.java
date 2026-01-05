482bbc43cddb49329b4f161552f8cacb23e980fd
snow___shower <wonsb25@gmail.com>
2026-01-05 15:39:43 +0900

    feat: 휴일, 평일 객체 작성

---
+++ src/main/java/oncall/domain/DayOff.java
@@ -0,0 +1,15 @@
+package oncall.domain;
+
+import java.util.List;
+
+public class DayOff {
+    private final List<String> dayoffList;
+
+    public DayOff(List<String> dayoffList){
+        this.dayoffList=dayoffList;
+    }
+
+    public List<String> getDayoffList(){
+        return dayoffList;
+    }
+}
---
+++ src/main/java/oncall/domain/Weekday.java
@@ -0,0 +1,15 @@
+package oncall.domain;
+
+import java.util.List;
+
+public class Weekday {
+    private final List<String> weekdayList;
+
+    public Weekday(List<String> weekdayList){
+        this.weekdayList=weekdayList;
+    }
+
+    public List<String> getWeekdayList(){
+        return weekdayList;
+    }
+}
