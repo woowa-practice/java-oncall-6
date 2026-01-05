55a0c15b0efac99c5e3e06b5aee28bc0021223e3
snow___shower <wonsb25@gmail.com>
2026-01-05 15:09:37 +0900

    feat: 입출력 기본 양식 작성

---
+++ src/main/java/oncall/view/InputView.java
@@ -0,0 +1,18 @@
+package oncall.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String inputMonthInfo() {
+        return Console.readLine();
+    }
+
+    public String inputWeekdayList() {
+        return Console.readLine();
+    }
+
+    public String inputDayOffList() {
+        return Console.readLine();
+    }
+}
