d472cdca256913f30b5563e222719a5d07b68d3c
snow___shower <wonsb25@gmail.com>
2026-01-05 21:34:09 +0900

    refactor: 컨트롤러, 출력 양식 재작성 및 메인 애플리케이션 조립

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,10 +1,17 @@
 package oncall;
 
+import oncall.controller.OncallController;
 import oncall.service.OncallService;
 import oncall.view.InputView;
+import oncall.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
+        InputView inputView=new InputView();
+        OutputView outputView=new OutputView();
+        OncallService oncallService=new OncallService();
+        OncallController oncallController=new OncallController(inputView, outputView, oncallService);
 
+        oncallController.run();
     }
 }
